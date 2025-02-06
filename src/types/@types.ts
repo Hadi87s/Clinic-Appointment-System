5187022d6c6a020aedc9164c461f1104e29c7423
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-02-06 11:25:41 +0200

    feat: create appointment screen with UI and form handling

--- src/types/@types.ts
+++ src/types/@types.ts
@@ -1,4 +1,7 @@
+import { Dayjs } from "dayjs";
+
 export interface IUser {
+  id: string;
   email: string;
   password: string;
   role: Role;
@@ -7,4 +10,32 @@ export interface IUser {
 export enum Role {
   patient = "Patient",
   doctor = "Doctor",
+}
+
+export enum AppointmentStatus {
+  PENDING = "pending",
+  CONFIRMED = "confirmed",
+  COMPLETED = "completed",
+}
+
+export interface Appointment {
+  id: string;
+  patientId: string; // Links to User.id
+  patientName: string;
+  contact: string;
+  age: number;
+  gender: string;
+  bookedSlot: string;
+  symptoms: string;
+  status: AppointmentStatus;
+  notes?: string; // Optional doctor notes
+}
+
+export interface FormData {
+  patientName: string;
+  contact: string;
+  age: number;
+  gender: string;
+  appointmentDateTime: Dayjs | null;
+  symptoms: string;
 }
