87f2eefb0735ba4a1f05794ffa1c6f4646cd558a
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-02-09 17:21:57 +0200

    feat: creating the navbar and adding responsiveness

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,16 +2,22 @@ import { Link, useNavigate } from "react-router-dom";
 import "./navbar.css";
 import "../../App.css";
 import { authContext } from "../../providers/authProvider";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Role } from "../../types/@types";
+import { Menu } from "lucide-react";
 const Navbar = () => {
   const { user, logout } = useContext(authContext);
+  const [menuVisible, setMenuVisible] = useState(false);
   const navigate = useNavigate();
   return (
-    <div className="container mt-3">
-      <div className="navbar rounded-2xl">
-        <div className="logo">Logo</div>
-        <div className="links">
+    <div className="flex justify-center mx-auto w-[100%]">
+      <div
+        className={`mt-2 flex w-[90%] justify-between content-center ${
+          menuVisible ? "h-45" : "h-15"
+        } p-4 text-white navbar rounded-2xl bg-gradient-to-r from-gray-900/85 to-gray-700/85 backdrop-blur-sm fixed inset-0 inset-x-auto`}
+      >
+        <div className="logo ">Logo</div>
+        <div className="links hidden md:flex gap-5 ">
           <Link to="/">Home</Link>
           {user?.role === Role.doctor ? (
             <Link to="/dashboard">Dashboard</Link>
@@ -23,7 +29,25 @@ const Navbar = () => {
           <Link to="/create-appointment">Create Appointment</Link>
           <Link to="/view-appointment">View Appointments</Link>
         </div>
-        <div className="log">
+
+        <div
+          className={`${
+            menuVisible ? "flex" : "hidden"
+          } flex-col mt-10 text-center gap-2`}
+        >
+          <Link to="/">Home</Link>
+          {user?.role === Role.doctor ? (
+            <Link to="/dashboard">Dashboard</Link>
+          ) : null}
+          {user?.role === Role.doctor ? (
+            <Link to="/manage-appointments">Manage Appointment</Link>
+          ) : null}
+
+          <Link to="/create-appointment">Create Appointment</Link>
+          <Link to="/view-appointment">View Appointments</Link>
+          <span></span>
+        </div>
+        <div className="log bg-gray-700 p-4 hidden md:flex items-center rounded-2xl ring-1 hover:ring-gray-400 hover:bg-gray-500 hover:text-gray-950 transition duration-200 cursor-pointer">
           {!user ? (
             <Link to="/login">Login</Link>
           ) : (
@@ -38,6 +62,15 @@ const Navbar = () => {
               Logout
             </Link>
           )}
+        </div>
+        <div
+          onClick={() => {
+            menuVisible ? setMenuVisible(false) : setMenuVisible(true);
+          }}
+          className="flex md:hidden"
+        >
+          {" "}
+          <Menu />
         </div>
       </div>
     </div>
