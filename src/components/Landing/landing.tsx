7be6b52eedc086cfce3caa625d265c0e10027650
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-02-09 18:53:31 +0200

    finishing the hero section.

--- src/components/Landing/landing.tsx
+++ src/components/Landing/landing.tsx
@@ -1,6 +1,45 @@
+import { Link } from "react-router-dom";
 import "../../App.css";
 const Landing = () => {
-  return <div className="container">Landing Page</div>;
+  return (
+    <div
+      className="w-[100%] h-[100vh] absolute inset-0"
+      style={{
+        backgroundImage: `url(${"../../../public/landingBackground.svg"})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="flex flex-row">
+        <div className="title mt-[250px] ml-15">
+          <h1 className="text-7xl text-gray-800 mb-5">
+            Your Health, Our Priority
+          </h1>
+          <p className="text-2xl text-gray-700 max-w-2xl ml-5">
+            Book your clinic appointments effortlessly. With a few clicks,
+            schedule, manage, and view your appointments anytime, anywhere.
+          </p>
+          <div className="flex justify-center max-w-2xl mt-10">
+            <Link
+              to="/create-appointment"
+              className="p-4 bg-blue-600 text-gray-50 rounded-2xl outline-2 outline-white"
+            >
+              Book an Appointment
+            </Link>
+          </div>
+        </div>
+        <div
+          className="hidden lg:block w-[500px] h-[730px]"
+          style={{
+            backgroundImage: `url(${"../../../public/doctor.png"})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+      </div>
+      <div className="doctorImage"></div>
+    </div>
+  );
 };
 
 export default Landing;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./navbar.css";
 import "../../App.css";
 import { authContext } from "../../providers/authProvider";
@@ -9,25 +9,70 @@ const Navbar = () => {
   const { user, logout } = useContext(authContext);
   const [menuVisible, setMenuVisible] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
   return (
     <div className="flex justify-center mx-auto w-[100%]">
       <div
         className={`mt-2 flex w-[90%] justify-between content-center ${
           menuVisible ? "h-45" : "h-15"
-        } p-4 text-white navbar rounded-2xl bg-gradient-to-r from-gray-900/85 to-gray-700/85 backdrop-blur-sm fixed inset-0 inset-x-auto`}
+        } p-4 z-10 text-blue-200 navbar rounded-2xl bg-gradient-to-r from-blue-900/85 to-blue-500/85
+        backdrop-blur-[2px] fixed inset-0 inset-x-auto border-2 border-blue-600`}
       >
         <div className="logo ">Logo</div>
         <div className="links hidden md:flex gap-5 ">
-          <Link to="/">Home</Link>
+          <Link
+            className={`transition duration-150 hover:text-white ${
+              location.pathname == "/" ? "border-b-2 rounded-b-[2px]" : ""
+            }`}
+            to="/"
+          >
+            Home
+          </Link>
           {user?.role === Role.doctor ? (
-            <Link to="/dashboard">Dashboard</Link>
+            <Link
+              className={`transition duration-150 hover:text-white ${
+                location.pathname == "/dashboard"
+                  ? "border-b-2 rounded-b-[2px]"
+                  : ""
+              }`}
+              to="/dashboard"
+            >
+              Dashboard
+            </Link>
           ) : null}
           {user?.role === Role.doctor ? (
-            <Link to="/manage-appointments">Manage Appointment</Link>
+            <Link
+              className={`transition duration-150 hover:text-white ${
+                location.pathname == "/manage-appointments"
+                  ? "border-b-2 rounded-b-[2px]"
+                  : ""
+              }`}
+              to="/manage-appointments"
+            >
+              Manage Appointment
+            </Link>
           ) : null}
 
-          <Link to="/create-appointment">Create Appointment</Link>
-          <Link to="/view-appointment">View Appointments</Link>
+          <Link
+            className={`transition duration-150 hover:text-white ${
+              location.pathname == "/create-appointment"
+                ? "border-b-2 rounded-b-[2px]"
+                : ""
+            }`}
+            to="/create-appointment"
+          >
+            Create Appointment
+          </Link>
+          <Link
+            className={`transition duration-150 hover:text-white ${
+              location.pathname == "/view-appointment"
+                ? "border-b-2 rounded-b-[2px]"
+                : ""
+            }`}
+            to="/view-appointment"
+          >
+            View Appointments
+          </Link>
         </div>
 
         <div
@@ -47,7 +92,7 @@ const Navbar = () => {
           <Link to="/view-appointment">View Appointments</Link>
           <span></span>
         </div>
-        <div className="log bg-gray-700 p-4 hidden md:flex items-center rounded-2xl ring-1 hover:ring-gray-400 hover:bg-gray-500 hover:text-gray-950 transition duration-200 cursor-pointer">
+        <div className="font-[500] bg-blue-700 p-4 hidden md:flex items-center rounded-2xl ring-1 hover:ring-blue-400 hover:bg-blue-500 hover:text-blue-950 transition duration-200 cursor-pointer">
           {!user ? (
             <Link to="/login">Login</Link>
           ) : (
