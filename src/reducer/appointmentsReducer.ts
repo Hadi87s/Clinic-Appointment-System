72e6850d2ff95d13b242dbad917492fe14568a1b
ahmad-anini <ahmadanini86@gmail.com>
2025-02-10 01:56:45 +0200

    feat : add update appointment case

--- src/reducer/appointmentsReducer.ts
+++ src/reducer/appointmentsReducer.ts
@@ -2,19 +2,23 @@ import { Appointment } from "../types/@types";
 
 export enum AppointmentActionKind {
   ADD = "ADD_APPOINTMENT",
+  UPDATE = "UPDATE_APPOINTMENT",
 }
 
 export type AppointmentState = {
   appointments: Appointment[];
 };
+
 export const INITIAL_APPOINTMENT: AppointmentState = {
   appointments: [],
 };
 
-export type AppointmentAction = {
-  type: AppointmentActionKind;
-  payload: { appointment: Appointment };
-};
+export type AppointmentAction =
+  | { type: AppointmentActionKind.ADD; payload: { appointment: Appointment } }
+  | {
+      type: AppointmentActionKind.UPDATE;
+      payload: { id: string; updatedData: Partial<Appointment> };
+    };
 
 export const appointmentReducer = (
   state: AppointmentState,
@@ -26,6 +30,17 @@ export const appointmentReducer = (
         ...state,
         appointments: [...state.appointments, action.payload.appointment],
       };
+
+    case AppointmentActionKind.UPDATE:
+      return {
+        ...state,
+        appointments: state.appointments.map((appointment) =>
+          appointment.id === action.payload.id
+            ? { ...appointment, ...action.payload.updatedData }
+            : appointment
+        ),
+      };
+
     default:
       return state;
   }
