94df627c50098f560e52de45c0752a47d1a81c1c
ahmad-anini <ahmadanini86@gmail.com>
2025-02-10 21:34:32 +0200

    fix : use enum AppointmentStatus instead of string

--- src/hooks/useFilteredAppointments.ts
+++ src/hooks/useFilteredAppointments.ts
@@ -1,4 +1,4 @@
-import { Appointment } from "../screens/viewAppointment";
+import { Appointment } from "../types/@types";
 
 const useFilteredAppointments = (
   appointments: Appointment[],
@@ -7,7 +7,8 @@ const useFilteredAppointments = (
 ): Appointment[] => {
   return appointments.filter(
     (appointment) =>
-      (statusFilter === "All" || appointment.status === statusFilter) &&
+      (statusFilter === "All" ||
+        appointment.status.toString() === statusFilter) &&
       appointment.patientName.toLowerCase().includes(searchQuery.toLowerCase())
   );
 };
